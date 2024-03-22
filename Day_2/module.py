f18d2f29dda78394ad8021e0ee38f91ecec450ba
Abhishek9995 <139108388+Abhishek9995@users.noreply.github.com>
2024-03-22 18:31:38 +0530

    Add files via upload

---
+++ Day_2/module.py
@@ -0,0 +1,15 @@
+# """Hello World"""
+# name="abhi"
+
+# def new(a,b):
+   
+#     """hyuhghg World"""
+#     return  a+b
+
+
+# def functionh(name,city):
+#     print(f"Hello,{name} {city}")
+
+# functionh("Abhishek","chennai")
+# functionh(city="kasarkod",name="Abhi")
+
